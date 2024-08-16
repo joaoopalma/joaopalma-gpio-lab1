959a7d4135ebef786f115bc54716fa8d216c4aeb
joaoopalma <joaoogp@al.insper.edu.br>
2024-08-16 10:31:15 -0300

    ex3 ex4 done

--- exe3/main.c
+++ exe3/main.c
@@ -2,9 +2,41 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int LED_PIN_R = 4;
+const int LED_PIN_G = 6;
+const int BTN_PIN_R = 28;
+const int BTN_PIN_G = 26;
+
 int main() {
   stdio_init_all();
 
+  gpio_init(LED_PIN_R);
+  gpio_init(LED_PIN_G);
+  gpio_init(BTN_PIN_R);
+  gpio_init(BTN_PIN_G);
+
+  gpio_set_dir(LED_PIN_R, GPIO_OUT);
+  gpio_set_dir(LED_PIN_G, GPIO_OUT);
+  gpio_set_dir(BTN_PIN_R, GPIO_IN);
+  gpio_set_dir(BTN_PIN_G, GPIO_IN);
+
+  gpio_pull_up(BTN_PIN_R);
+  gpio_pull_up(BTN_PIN_G);
+
   while (true) {
+  if (!gpio_get(BTN_PIN_R)) {
+    int luz = gpio_get(LED_PIN_R);
+    gpio_put(LED_PIN_R, !luz);
+    sleep_ms(50);
+    while (!gpio_get(BTN_PIN_R)) {
+    };
+  };
+  if (!gpio_get(BTN_PIN_G)) {
+    int luz = gpio_get(LED_PIN_G);
+    gpio_put(LED_PIN_G, !luz);
+    sleep_ms(50);
+    while (!gpio_get(BTN_PIN_G)) {
+    }
   }
+}
 }
--- exe4/main.c
+++ exe4/main.c
@@ -2,10 +2,48 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int LED_PIN_R = 5;
+const int LED_PIN_P = 8;
+const int LED_PIN_B = 11;
+const int LED_PIN_Y = 15;
+const int BTN_PIN_R = 28;
+
+
 int main() {
   stdio_init_all();
 
+  gpio_init(LED_PIN_R);
+  gpio_init(LED_PIN_P);
+  gpio_init(LED_PIN_B);
+  gpio_init(LED_PIN_Y);
+  gpio_init(BTN_PIN_R);
+
+  gpio_set_dir(LED_PIN_R, GPIO_OUT);
+  gpio_set_dir(LED_PIN_P, GPIO_OUT);
+  gpio_set_dir(LED_PIN_B, GPIO_OUT);
+  gpio_set_dir(LED_PIN_Y, GPIO_OUT);
+  gpio_set_dir(BTN_PIN_R, GPIO_IN);
+
+  gpio_pull_up(BTN_PIN_R);
+
   while (true) {
+    if (!gpio_get(BTN_PIN_R)) {
+      gpio_put(LED_PIN_R, 1);
+      sleep_ms(300);
+      gpio_put(LED_PIN_R, 0);
+      gpio_put(LED_PIN_P, 1);
+      sleep_ms(300);
+      gpio_put(LED_PIN_P, 0);
+      gpio_put(LED_PIN_B, 1);
+      sleep_ms(300);
+      gpio_put(LED_PIN_B, 0);
+      gpio_put(LED_PIN_Y, 1);
+      sleep_ms(300);
+      gpio_put(LED_PIN_Y, 0);
+
+      while (!gpio_get(BTN_PIN_R)) {
+      };
+    };
     // Use delay de 300 ms entre os estados!
   }
 }
